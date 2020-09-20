111587e1933929648eba1614b7a9478e0fe5eac8
kalamgit143 <66198415+kalamgit143@users.noreply.github.com>
2020-09-20 16:28:58 +0530

    Update Test2.java

--- src/main/java/mypack/Test2.java
+++ src/main/java/mypack/Test2.java
@@ -2,8 +2,9 @@ package mypack;
 
 public class Test2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		System.out.println("i changed code");
 
 	}
 
